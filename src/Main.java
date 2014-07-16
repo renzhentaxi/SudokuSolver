5507ab0b109ec29488a4d2a44a2e8b86e9a153a4
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-07-15 22:05:59 -0400

    added Grid

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        Grid board = new Grid( 9, 9);
+        board.set(0,0,new Box(3));
+        board.set(0,1,new Box(5));
+        Grid newBoard =  board.getSubGrid(0,0,1,2);
+        System.out.println(newBoard);
     }
 
 }
