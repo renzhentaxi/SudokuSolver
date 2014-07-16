4814b0aa6cd1efbdf5b95a37d8bd415def5df58a
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-07-15 22:30:29 -0400

    starting the gui

---
+++ src/Box.java
@@ -0,0 +1,19 @@
+
+public class Box
+{
+    private int value;
+    public Box()
+    {
+
+    }
+
+    public Box(int value)
+    {
+        this.value = value;
+    }
+
+    public String toString()
+    {
+        return Integer.toString(value);
+    }
+}
--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,41 @@
+import java.util.Arrays;
+
 public class Grid
 {
-  
+    private Box[][] GRID;
+
+    public Grid( int columnCount, int rowCount)
+    {
+        GRID = new Box[columnCount][rowCount];
+    }
+
+    public Box get(int x, int y)
+    {
+        return GRID[x][y];
+    }
+
+    public void set(int x, int y, Box b)
+    {
+        GRID[x][y] = b;
+    }
+
+    public Grid getSubGrid(int x, int y, int length, int height)
+    {
+        Grid subGrid = new Grid(length, height);
+        int x2 = x + length;
+        int y2 = y + height;
+        for (;x < x2; x += 1)
+        {
+            for(; y < y2; y += 1)
+            {
+                subGrid.set(x,y,get(x,y));
+            }
+        }
+        return subGrid;
+    }
+
+    public String toString()
+    {
+       return Arrays.deepToString(GRID);
+    }
+}
